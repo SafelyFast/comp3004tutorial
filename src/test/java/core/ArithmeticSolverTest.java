8898f33ffb488c458b8988375bf53916c728dbc6
Jacob <jacobisaac1997@gmail.com>
2018-09-08 11:07:01 -0400

    main: added ArithmeticSolver class implementation

--- src/test/java/core/ArithmeticSolverTest.java
+++ src/test/java/core/ArithmeticSolverTest.java
@@ -7,34 +7,34 @@ public class ArithmeticSolverTest extends TestCase{
 	{
 		ArithmeticSolver solver = new ArithmeticSolver();
 
-		assertEquals(4, solver.add(2,2));
+		assertEquals(4.0, solver.add(2,2));
 	}
 	
 	public void testSubtract()
 	{
 		ArithmeticSolver solver = new ArithmeticSolver();
 
-		assertEquals(0, solver.subtract(2,2));
+		assertEquals(0.0, solver.subtract(2,2));
 	}
 	
 	public void testMultiply()
 	{
 		ArithmeticSolver solver = new ArithmeticSolver();
 
-		assertEquals(4, solver.multiply(2,2));
+		assertEquals(4.0, solver.multiply(2,2));
 	}
 	
 	public void testDivide()
 	{
 		ArithmeticSolver solver = new ArithmeticSolver();
 
-		assertEquals(1, solver.divide(2,2));
+		assertEquals(1.0, solver.divide(2,2));
 	}
 	
 	public void testModulus()
 	{
 		ArithmeticSolver solver = new ArithmeticSolver();
 
-		assertEquals(0, solver.modulus(2,2));
+		assertEquals(0.0, solver.modulus(2,2));
 	}
 }
